322f8fdee30b21535988ee4b03a4a50a0a64a0f3
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2022-08-14 19:41:11 -0500

    Revert "Adding more X Algos"
    This reverts commit 764794497aa5fc4c93f9eb0da9bae35f203e8393.

--- src/Miningcore/Crypto/Hashing/Algorithms/X13BCD.cs
+++ src/Miningcore/Crypto/Hashing/Algorithms/X13BCD.cs
@@ -1,22 +1,21 @@
-using System;
-using Cybercore.Contracts;
-using Cybercore.Native;
+using Miningcore.Contracts;
+using Miningcore.Native;
 
-namespace Cybercore.Crypto.Hashing.Algorithms
+namespace Miningcore.Crypto.Hashing.Algorithms;
+
+[Identifier("x13bcd")]
+public unsafe class X13BCD : IHashAlgorithm
 {
-    public unsafe class X13BCD : IHashAlgorithm
+    public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
     {
-        public void Digest(ReadOnlySpan<byte> data, Span<byte> result, params object[] extra)
-        {
-            Contract.Requires<ArgumentException>(data.Length == 80, $"{nameof(data)} must be exactly 80 bytes long");
-            Contract.Requires<ArgumentException>(result.Length >= 32, $"{nameof(result)} must be greater or equal 32 bytes");
+        Contract.Requires<ArgumentException>(data.Length == 80);
+        Contract.Requires<ArgumentException>(result.Length >= 32);
 
-            fixed (byte* input = data)
+        fixed (byte* input = data)
+        {
+            fixed (byte* output = result)
             {
-                fixed (byte* output = result)
-                {
-                    LibMultihash.x13_bcd(input, output);
-                }
+                Multihash.x13_bcd(input, output);
             }
         }
     }
